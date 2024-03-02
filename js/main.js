4ff0ce962fb9a171ed7c1eeb3020e29928311900
Abderrahmendr <160425292+Abderrahmendr@users.noreply.github.com>
2024-03-02 19:27:45 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,282 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName('skills-content'),
+      skillsHeader = document.querySelectorAll('.skills-header')
+     
+      function toggleSkills(){
+        let itemClass = this.parentNode.className
+
+        for(i = 0; i < skillsContent.length; i++){
+            skillsContent[i].className = 'skills-content skills-close'
+        }
+        if(itemClass === 'skills-content skills-close'){
+            this.parentNode.className = 'skills-content skills-open'
+        }
+      }
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+
+/*==================== SERVICES MODAL ====================*/
+
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+  document.body.classList.add("disable-scroll");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+      document.body.classList.remove("disable-scroll");
+    });
+  });
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*==================== DARK LIGHT THEME ====================*/
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+//validate if user previously chose a theme
+if (selectedTheme) {
+  // if theme selected by user previously then we add/remove classes again based on localStorage
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+//if initially there is no local storage ie. user has not made a choice and this is first time loading
+//then we check if browser/OS is in dark mode and then add dark theme if required by default
+else if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  console.log("found dark mode for browser/OS");
+  // add dark theme by setting dark theme flags in localStorage
+  localStorage.setItem("selected-theme", "dark");
+  localStorage.setItem("selected-icon", "uil-moon");
+  // add classes for dark theme in DOM
+  document.body.classList.add(darkTheme);
+  themeButton.classList.add(iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*==================== DARK LIGHT THEME ====================*/
+const dropdown = document.querySelector('.drop-down');
+const list = document.querySelector('.list');
+const selected = document.querySelector('.selected');
+const selectedImg = document.querySelector('.selectedImg');
+function selectFlag(language) {
+  const imgSrc = `img/${language}.png`;
+  selected.innerHTML = `<img src="${imgSrc}" alt="${language}" />`;
+  list.classList.remove('show');
+}
+
+ dropdown.addEventListener('click', ()=>{
+  list.classList.toggle('show');
+ })
+ list.addEventListener('click', (e) =>{
+  const img = e.target.querySelector('img');
+  selectedImg.src = img.src;
+  selected.innerHTML = text.innerHTML;
+ }) 
+
+ 
+ 
+// (English version)
+function switchToGerman() {
+  window.location.href = "index_de.html";
+}
+
+//  (German version)
+function switchToEnglish() {
+  window.location.href = "index.html";
+}
+ window.addEventListener('load', function () {
+  document.getElementById('loading').style.display = 'none';
+}); 
+/*==================== DARK LIGHT THEME ====================*/
+const text = document.querySelector(".sec-text");
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Servus, ich bin";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Hi, i'm ";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Bonjour, je suis";
+    }, 8000); 
+}
+textLoad();
+setInterval(textLoad, 12000);
+
+
+
+// JavaScript code to handle form submission
+document.addEventListener('DOMContentLoaded', function () {
+  const contactForm = document.getElementById('contactForm');
+
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    // Collect form data
+    const formData = new FormData(contactForm);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
+
+    // Send form data to backend server
+    sendData(formDataObject);
+  });
+
+  function sendData(formData) {
+    // Example: Send form data using Fetch API
+    fetch('your-backend-url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Form submission successful:', data);
+        // Optionally, display a success message to the user
+      })
+      .catch(error => {
+        console.error('There was a problem with the form submission:', error);
+        // Optionally, display an error message to the user
+      });
+  }
+});
